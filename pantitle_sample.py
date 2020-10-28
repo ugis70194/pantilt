80602fb9a8d440d5181b58e3421885561b9f90e5
ugis70194 <ugis70194@yahoo.co.jp>
2020-10-28 11:50:19 +0900

    ok

--- pantitle_sample.py
+++ pantitle_sample.py
@@ -3,23 +3,28 @@ import time
 
 import pantilthat
 
+EPS = 0.000001
 
 while True:    
   X, Y = input().split()
   X = int(X)
   Y = int(Y)
 
-  r = math.sqrt((X-1) ** 2 + (Y - 1) ** 2) 
-  eps = 0.000001
 
-  alpha = math.atan((Y-1)/(X-1+eps)) * (180/math.acos(-1)) 
+  r = math.sqrt(X ** 2 + Y ** 2) 
   
-  theta = 90/(1 - math.log(1/(r+1)) - eps)
+  alpha = 0
+  theta = 0
+
+  alpha = math.atan((Y)/(X+EPS)) * (180/math.acos(-1))  
+  if alpha > 0: alpha = min(alpha, 90)
+  else: alpha = max(alpha, -90)
+  
+  theta = 90/(1 - math.log(1/(r+1)) - EPS)
+  theta = min(theta, 90)
 
   print(alpha)
   print(theta)
 
   pantilthat.pan(alpha)
   pantilthat.tilt(theta)
-
-  time.sleep(0.005)
